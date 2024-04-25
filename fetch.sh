0ba7054856e8dd2e042b92d3744a44766e59ee7a
Yukari Chiba <i@0x7f.cc>
2024-04-25 11:16:51 +0800

    init

---
+++ fetch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+REPO=deepin-community/deepin-riscv-kernel
+
+mkdir -p tmp
+
+pushd tmp
+
+ENDP=https://api.github.com/repos/$REPO/actions/artifacts
+
+WFID=`curl $ENDP | jq -c '.artifacts | first | .workflow_run | .id'`
+
+for device in mango star64 th1520 unmatched visionfive2-61y visionfive2 visionfive2-upstream; do
+  wget https://nightly.link/$REPO/actions/runs/$WFID/kernel-$device-deb.zip
+  unzip ./kernel-$device-deb.zip
+done
+
+popd
+
+reprepro -b repo/ includedeb beige tmp/*.deb
+
+rm -r tmp
